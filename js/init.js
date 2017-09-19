76153d980b4bfbd88c368d86115d117852aaf1ad
David Lac <kiddmit3@gmail.com>
2017-09-19 07:32:42 -0700

    template

---
+++ js/init.js
@@ -0,0 +1,17 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+
+  $(document).ready(function(){
+      $('.carousel').carousel();
+	});
+
+  $(document).ready(function(){
+      $('.collapsible').collapsible();
+    });  
+
+})(jQuery); // end of jQuery name space
