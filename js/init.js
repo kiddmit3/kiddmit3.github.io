3ab9f83699fae693e64471a976b6cb756a9e3d8f
David Lac <kiddmit3@gmail.com>
2018-06-11 15:24:41 -0700

    included user and password info

--- js/init.js
+++ js/init.js
@@ -6,7 +6,7 @@ var portfolioPieces = [{
         technologies: ["html5", "css", "jquery", "materialize"]
     },
     {
-        name: "PLW Tutoring",
+        name: "Premier Education Tutoring",
         description: "Built for Premier Education, this is a customized React application that organizes schedules for tutors and students.",
         URL: "https://kiddmit3.github.io/Premier-Academy-Arcadia/",
         image: "./assets/img/portfolio/PLW.png",
@@ -42,8 +42,8 @@ var portfolioPieces = [{
         technologies: ["html5", "css", "jquery", "bootstrap"]
     },
     {
-        name: "Plastic Boulders",
-        description: "A full stack application that allows rock climbing gym-goers to document and share their climbs, past and present. Create an account, upload images, and contribute to the database of climbing routes.",
+        name: "βouldersapp™",
+        description: "A full stack application that allows rock climbing gym-goers to document and share their climbs, past and present. Create an account, upload images/videos, and contribute to the database of climbing routes. But most of all, view the solutions to the routes you are struggling with at the gym! Log in with username: 'demo' and password: 'password' to see all the features including QR codes, membership checking, and personal statistics.",        
         URL: "https://plasticboulders.herokuapp.com/",
         image: "./assets/img/portfolio/plasticb.jpg",
         technologies: ["html5", "css", "node", "javascript", "expressjs", "mongodb", "bootstrap"]
